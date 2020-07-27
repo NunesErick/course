dd39373cf9ec1b9801f637c860f547eccd9cb9e8
Érick Nunes <erick.szns@hotmail.com>
2020-07-27 13:39:39 -0300

    Adicionado Update ao product

--- src/main/java/com/course/portf/resources/ProductResource.java
+++ src/main/java/com/course/portf/resources/ProductResource.java
@@ -1,13 +1,18 @@
 package com.course.portf.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.course.portf.entities.Product;
 import com.course.portf.services.ProductService;
@@ -29,5 +34,16 @@ public class ProductResource {
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
 		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product prod) {
+		service.insertUser(prod);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(prod.getId()).toUri();
+		return ResponseEntity.created(uri).body(prod);
+	}
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product prod){
+		prod = service.update(id, prod);
+		return ResponseEntity.ok().body(prod);
 	}
 }
--- src/main/java/com/course/portf/services/ProductService.java
+++ src/main/java/com/course/portf/services/ProductService.java
@@ -3,11 +3,14 @@ package com.course.portf.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.course.portf.entities.Product;
 import com.course.portf.repositories.ProductRepository;
+import com.course.portf.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,5 +24,25 @@ public class ProductService {
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public Product insertUser(Product prod) {
+		return repository.save(prod);
+	}
+	
+	public Product update(Long id,Product prod) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity,prod);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	private void updateData(Product prod,Product uprod) {
+		prod.setName(uprod.getName());
+		prod.setPrice(uprod.getPrice());
+		prod.setDescription(uprod.getDescription());
+		prod.setImgUrl(uprod.getImgUrl());
 	}
 }
